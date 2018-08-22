ed48feff393890239000b31e5a167aa2571181a4
Athan Clark <athan.clark@gmail.com>
2018-08-21 19:27:44 -0600

    generic instance for json

--- src/Data/Argonaut.purs
+++ src/Data/Argonaut.purs
@@ -3,7 +3,6 @@ module Data.Argonaut
   , module Data.Argonaut.Decode
   , module Data.Argonaut.Encode
   , module Data.Argonaut.JCursor
-  , module Data.Argonaut.Parser
   , module Data.Argonaut.Prisms
   , module Data.Argonaut.Traversals
   ) where
@@ -12,6 +11,5 @@ import Data.Argonaut.Core (JArray, JAssoc, JBoolean, JNull, JNumber, JObject, JS
 import Data.Argonaut.Decode (class DecodeJson, decodeJson, getField, (.?))
 import Data.Argonaut.Encode (class EncodeJson, assoc, encodeJson, extend, (:=), (~>))
 import Data.Argonaut.JCursor (JCursor(..), JsonPrim(..), cursorGet, cursorSet, downField, downIndex, fail, fromPrims, inferEmpty, insideOut, primBool, primNull, primNum, primStr, primToJson, runJsonPrim, toPrims)
-import Data.Argonaut.Parser (jsonParser)
 import Data.Argonaut.Prisms (_Array, _Boolean, _Null, _Number, _Object, _String)
 import Data.Argonaut.Traversals (_JsonArray, _JsonBoolean, _JsonNull, _JsonNumber, _JsonObject, _JsonString)
